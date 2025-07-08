29343e43924b62c65cf12eb0e5f7fab49407899f
SW-in-beta <eeokee52@gmail.com>
2025-07-08 09:23:59 +0900

    week15/BOJ3079.py

--- week15/3079(입국심사).py
+++ week15/3079(입국심사).py
@@ -1,23 +1,27 @@
 """
-심사관마다 시간이 다르다.
-항상 이동을 해야 하는 것은 아닌게 핵심
-이거 그냥 끝 시간을 기준으로 우선순위 큐이 넣어가면 되는거 아닌가? 아니 굳이 우선순위 큐가 필요한가?
-N, M이 매우 큼에 유의
-그냥 배수끼리 쭈욱 넣어가면 될 거 같은데
-T_k와 M이 10**9 다. 즉, 복잡도가 더 생기면 안된다는 의미
-
-아 잘못생각했다.. 심사관이 N이구나.
-그럼 N에 대해서는 정렬을 해줘도 된다. 
-만약 그렇게 정렬이 된 후에, 힙으로 넣고 꺼내면 안되려나? log10만인데
-일단 해보자.
+와... 이거 발상의 전환
+한 심사관이 특정 time 안에 처리할 수 있는 사람의 수 : time / t
+그럼 그걸 줄여가면서, 딱 특정 사람의 수만 처리할 수 있는 "시간"을 찾아야겠네. 미쳤다
+처음에는 가장 큰 걸로.
+left와 right가 같을 때 까지인가?
+종료 조건이 뭐지?
+만약 total이 딱 맞게 떨어진다면? 근데 그거보다 작을 수도 있잖아? ㅇㅋ
 """
 import sys
-from heapq import heappush, heappop, heapify
+
 input = lambda : sys.stdin.readline().rstrip()
 N, M = map(int, input().split())
-officers = [(int(input()), 1) for _ in range(N)]
-heapify(officers)
-for _ in range(M):
-    t, n = heappop(officers)
-    heappush(officers, (t // n * (n+1), n+1))
-print(t)
+T = [int(input()) for _ in range(N)]
+left = 1
+right = max(T) * M
+
+while left < right:
+    mid = (left + right) // 2
+    total = sum(mid // t for t in T)
+
+    if total >= M:
+        right = mid
+    else:
+        left = mid + 1
+
+print(left)
