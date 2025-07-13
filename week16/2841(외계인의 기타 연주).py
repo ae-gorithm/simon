2117df4256fddbcc9ddac92b207b472f3a36d546
SW-in-beta <eeokee52@gmail.com>
2025-07-13 09:57:39 +0900

    week16/BOJ2841.py

--- week16/2841(외계인의 기타 연주).py
+++ week16/2841(외계인의 기타 연주).py
@@ -2,18 +2,25 @@
 이렇게 풀긴 풀었는데, 그렇게 하면 시간 초과가 뜬다.
 이분 탐색으로 들어갈 자리를 찾는게 나을까?
 프렛의 수가 30만 이라는 점에서 문제가 생기는듯
+
+매번 슬라이싱 하는게 문제인가?
+
 """
+
+from bisect import bisect_right
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
 N, P = map(int, input().split())
 lines = [None] + [[0] for _ in range(6)]
 cnt = 0
 
 for _ in range(N):
-    line_index,  pret = map(int, input().split())
-    line = lines[line_index]
-    while line[-1] > pret:
+    line, fret = map(int, input().split())
+    pos = bisect_right(lines[line], fret)
+    cnt += len(lines[line]) - pos
+    lines[line] = lines[line][:pos]
+    if lines[line][-1] < fret:
         cnt += 1
-        line.pop()
-    if line[-1] < pret:
-        cnt += 1
-        line.append(pret)
+        lines[line].append(fret)
 print(cnt)
