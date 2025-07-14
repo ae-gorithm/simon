9c2568f39cda8602c2a515dc9c5ab8ef9c7fd358
SW-in-beta <eeokee52@gmail.com>
2025-07-15 08:31:16 +0900

    week16/BOJ1987.py 시간초과2

--- week16/1987(알파벳).py
+++ week16/1987(알파벳).py
@@ -1,26 +1,31 @@
 """
-그래프 탐색.
-백트래킹
+이걸 DP로 구한다면?
+어디에서 들어오는지만 중요한게 아니라 그냥 다 중요하니까. 이걸 뭐... -> 안될 것 같다.
+백트래킹인데, 반복문으로 풀어보자.
 """
 import sys
-sys.setrecursionlimit(10**6)
 input = lambda : sys.stdin.readline().rstrip()
 
 R, C = map(int, input().split())
 board = tuple(input() for _ in range(R))
 dx = (-1, 0, 0, 1)
 dy = (0, 1, -1, 0)
+stack = [(0, 0, set())]
 
 cnt = 0
-def backtracking(r, c, trace):
-    if board[r][c] in trace:
-        return len(trace)
-    trace.append(board[r][c])
-    cnt = 0
-    for x, y in zip(dx, dy):
-        if 0 <= r + x < R and 0 <= c + y < C:
-            cnt = max(cnt, backtracking(r+x, c+y, trace))
-    trace.pop()
-    return cnt
 
-print(backtracking(0, 0, []))
+# 반복문에서는 Trace 문제를 어떻게 해결할까.
+# stack마다 set을 저장해두긴 좀 그렇고. 그럴건뭐야
+while stack:
+    r, c, trace = stack.pop()
+    if board[r][c] in trace:
+        cnt = max(cnt, len(trace))
+        continue
+    trace.add(board[r][c])
+    next_step = [(r+x, c+y) for x, y in zip(dx, dy) if 0 <= r + x < R and 0 <= c + y < C and board[r+x][c+y] not in trace]
+    if len(next_step) == 0:
+        cnt = max(cnt, len(trace))
+    for nr, nc in next_step:
+        stack.append((nr, nc, set(trace)))
+            
+print(cnt)
