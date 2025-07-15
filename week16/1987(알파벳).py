d22c3368ecbb63d52e5b44bb0e4dfdc56321e691
SW-in-beta <eeokee52@gmail.com>
2025-07-15 09:02:05 +0900

    week16/BOJ1987.py 메모리초과

--- week16/1987(알파벳).py
+++ week16/1987(알파벳).py
@@ -1,31 +1,24 @@
 """
-이걸 DP로 구한다면?
-어디에서 들어오는지만 중요한게 아니라 그냥 다 중요하니까. 이걸 뭐... -> 안될 것 같다.
-백트래킹인데, 반복문으로 풀어보자.
+set이 아니라 비트마스킹으로 해보자.
+그리고 어떤 지점을 탐색했는지를 파악ㅇ르 해두자.
 """
 import sys
+sys.setrecursionlimit(10**6)
 input = lambda : sys.stdin.readline().rstrip()
 
 R, C = map(int, input().split())
-board = tuple(input() for _ in range(R))
+board = tuple(tuple(map(lambda x: 2 ** (ord(x)-65), input())) for _ in range(R))
 dx = (-1, 0, 0, 1)
 dy = (0, 1, -1, 0)
-stack = [(0, 0, set())]
 
-cnt = 0
-
-# 반복문에서는 Trace 문제를 어떻게 해결할까.
-# stack마다 set을 저장해두긴 좀 그렇고. 그럴건뭐야
-while stack:
-    r, c, trace = stack.pop()
-    if board[r][c] in trace:
-        cnt = max(cnt, len(trace))
-        continue
-    trace.add(board[r][c])
-    next_step = [(r+x, c+y) for x, y in zip(dx, dy) if 0 <= r + x < R and 0 <= c + y < C and board[r+x][c+y] not in trace]
-    if len(next_step) == 0:
-        cnt = max(cnt, len(trace))
+def backtracking(r, c, bit=0, cnt=0):
+    if board[r][c] & bit:
+        return cnt
+    bit |= board[r][c]
+    cnt += 1
+    next_step = [(r+x, c+y) for x, y in zip(dx, dy) if 0 <= r+x < R and 0 <= c+y < C]
     for nr, nc in next_step:
-        stack.append((nr, nc, set(trace)))
-            
-print(cnt)
+        new_cnt = max(cnt, backtracking(nr, nc, bit, cnt))
+    return new_cnt
+
+print(backtracking(0, 0))
